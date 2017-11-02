bd50f78e2e20030d21e759528ecb5cd6d092d2ed
Chandra <krsna.c@gmail.com>
2017-11-02 22:09:48 +0000

    made a loaded die

--- loadedDie.js
+++ loadedDie.js
@@ -1,9 +1,10 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  var i = -1;
 
   return function() {
-    /* your code here */
+    i++;
+    return list[i];
   }
 }
 
